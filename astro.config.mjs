e696fe74f3b000b0f0d697d26bf1f54f8229cafd
Aman Pradhan <pradhanaman2004@gmail.com>
2026-06-06 16:36:41 +0530

    production ready - 100/100 lighthouse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+
 
 export default defineConfig({
   site: 'https://dogbreedage.com',
   output: 'static',
+  
   devToolbar: {
     enabled: false,
   },
